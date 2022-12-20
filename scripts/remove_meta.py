2fbb01767b36615fa79acf8861c212ce0d61452f
Richard Chien <stdrc@outlook.com>
2022-12-21 00:13:28 +0800

    update scripts
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- scripts/remove_meta.py
+++ scripts/remove_meta.py
@@ -1,8 +1,17 @@
 import os
 import re
+import sys
 from PIL import Image
 
+begin_date = sys.argv[1]
+
 for dir, subdirs, files in os.walk("static/photos"):
+    if m := re.search(r"\d{4}-\d{2}-\d{2}", dir):
+        if m.group() < begin_date:
+            continue
+    else:
+        continue
+
     for file in files:
         if not re.search(r"\.(jpe?g|png)$", file.lower()) or re.search(
             r"thumb", file.lower()
